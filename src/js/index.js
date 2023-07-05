482eda1290e212ed31439a722e33261cdd2bd57f
Hamzahxou <135695076+Hamzahxou@users.noreply.github.com>
2023-07-05 09:19:29 +0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -135,258 +135,229 @@ if (localStorage.getItem("mengerti") == "y") {
     localStorage.setItem("mengerti", "y");
   });
 }
-//simpan data ke google sheet
 
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbzZciEdywHsr62lneaD_35hh7doKpe0GrvJGjf_4ntU3jh_3Xx2KvePlZJ1CDhoKNWp/exec";
-const form = document.forms["Forum"];
-const btnActive = document.querySelector("button.active");
-const btnDisable = document.querySelector("button.disable");
-const pesanForm = document.getElementById("pesan");
-
-const namaForm = document.getElementById("nama");
-const namaLocal = localStorage.getItem("nama");
-if (namaForm) {
-  namaForm.value = localStorage.getItem("nama");
-  //namaForm.setAttribute("disabled", "");
-} else {
-  namaForm.value = "";
-}
-
-if (!namaLocal) {
- namaForm.removeAttribute("disabled");
-} else {
-  
-}
-
-const informasi = document.querySelector(".informasi");
-
-function inf() {
-  informasi.parentNode.classList.remove("d-none");
-  setTimeout(() => {
-    informasi.parentNode.classList.add("d-none");
-  }, 5000);
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (namaForm.value == "") {
-    inf();
-    informasi.textContent = "namanya gada";
-  } else if (namaForm.value.length > 25) {
-    inf();
-    informasi.textContent = "nama tidak boleh lebih dari 25 kata.";
-  } else if (pesanForm.value == "") {
-    inf();
-    informasi.textContent = "mau ngirim apa coba";
-  } else if (pesanForm.value.length > 150) {
-    inf();
-    informasi.textContent = "Pesan tidak boleh lebih dari 150 kata.";
-  } else {
-    function getRandomColor() {
-      const colors = ["#ffc107", "#dc3545", "#17a2b8"];
-      const randomIndex = Math.floor(Math.random() * colors.length);
-      return colors[randomIndex];
-    }
-
-    const randomColor = getRandomColor();
-    const color = document.getElementById("color");
-    color.value = randomColor;
-
-    const pesanValue = pesanForm.value;
-
-    // Mengganti karakter enter (\n) dengan spasi dalam teks pesan
-    const cleanedPesanValue = pesanValue.replace(/[\n\r]/g, " ");
-
-    //ketika di submit
-    btnActive.classList.toggle("d-none");
-    btnDisable.classList.toggle("d-none");
-    const namaValue = namaForm.value;
-    localStorage.setItem("nama", namaValue);
-
-    // Menggunakan FormData untuk mengirim data melalui fetch
-    const formData = new FormData(form);
-    formData.set("pesan", cleanedPesanValue);
-
-    fetch(scriptURL, { method: "POST", body: formData })
-      .then((response) => {
-        btnActive.classList.toggle("d-none");
-        btnDisable.classList.toggle("d-none");
-        pesanForm.value = "";
-      }) //console.log("Success!", response)
-      .catch((error) => "ups ada yang salah 🤔"); //console.error("Error!", error.message)
-  }
-});
-
-//tampilkan
-const spreadsheetId = "1ZTCERAwio2qTbOKnyNpuZAYdcOZIuYpizSUDjswkJG0";
-let url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:csv`;
-
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-  fetchDataAndConvertToHTML();
-  setInterval(fetchDataAndConvertToHTML, 5000); // Refresh setiap 5 detik (5000 milidetik)
-}
-
-function fetchDataAndConvertToHTML() {
-  // Tambahkan parameter unik ke URL fetch
-  const uniqueParam = new Date().getTime();
-  const fetchUrl = `${url}&_=${uniqueParam}`;
-
-  fetch(fetchUrl)
-    .then((response) => response.text())
-    .then((data) => {
-      const html = convertDataToHTML(data);
-      const areaPesanMasuk = document.querySelector(".areaPesanMasuk");
-      areaPesanMasuk.innerHTML = html;
-    })
-    .catch((error) => {
-      console.log("Terjadi kesalahan:", error);
-    });
-}
-
-function convertDataToHTML(data) {
-  const rows = data.split("\n").slice(1);
-  const html = rows
-    .map((row) => {
-      const values = row.split('","').map((value) => value.replace(/"/g, ""));
-      const [timestamp, nama, pesan, color] = values;
-
-      // Validasi enter pada pesan
-      const escapedPesan = document.createElement("div");
-      escapedPesan.textContent = pesan; // Menghapus karakter enter (\n)
-
-      return `
-        <div class="pesanMasuk">
-          <div class="nama" style="--color: ${color};">${nama}</div>
-          <div class="pesan">${escapedPesan.innerHTML}</div>
-        </div>
-      `;
-    })
-    .join("");
-
-  return `<div class="areaPesanMasuk">${html}</div>`;
-}
-
-const human = document.querySelector(".human");
-let offsetX, offsetY;
-
-const move = (e) => {
-  human.style.left = `${e.clientX - offsetX}px`;
-  human.style.top = `${e.clientY - offsetY}px`;
-};
-
-human.addEventListener("dblclick", () => {
-  human.style.position = "absolute";
-  human.style.cursor = "move";
-});
-
-human.addEventListener("mousedown", (b) => {
-  offsetX = b.clientX - human.offsetLeft;
-  offsetY = b.clientY - human.offsetTop;
-  document.addEventListener("mousemove", move);
-});
-
-document.addEventListener("mouseup", () => {
-  document.removeEventListener("mousemove", move);
-});
-
-human.addEventListener("touchstart", (event) => {
-  const touch = event.touches[0];
-  offsetX = touch.clientX - human.offsetLeft;
-  offsetY = touch.clientY - human.offsetTop;
-});
-
-human.addEventListener("touchmove", (event) => {
-  event.preventDefault(); // Mencegah scroll halaman saat menggeser objek
-  const touch = event.touches[0];
-  human.style.left = touch.clientX - offsetX + "px";
-  human.style.top = touch.clientY - offsetY + "px";
-});
-
-human.addEventListener("touchend", () => {
-  // Tindakan setelah sentuhan selesai
-});
-
-const imgElement = document.querySelector("img.human");
-
-imgElement.addEventListener("click", (event) => {
-  event.preventDefault();
-});
-
-function getRandomPosition() {
-  const minX = 20; // Minimum x position (left)
-  const maxX = window.innerWidth - 70; // Maximum x position (right)
-  const randomX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
-  return randomX;
-}
-
-function createKucing() {
-  const minSize = 20;
-  const maxSize = 50;
-  // Menghasilkan ukuran acak antara minSize dan maxSize
-  let randomSize = Math.floor(
-    Math.random() * (maxSize - minSize + 1) + minSize
-  );
-  const kucing = document.createElement("img");
-  kucing.setAttribute("src", "src/img/Kucing.gif");
-  kucing.setAttribute("alt", "kucing");
-  kucing.classList.add("kucing");
-  kucing.classList.add("trans");
-  const randomX = getRandomPosition();
-  kucing.style.left = randomX + "px";
-  kucing.style.width = `${randomSize}px`;
-  let offsetX, offsetY;
-  const move = (e) => {
-    kucing.style.left = `${e.clientX}px`;
-    kucing.style.top = `${e.clientY}px`;
+//karena banyak yang melakukan tes deven:v
+const _0x201438 = _0x26b6;
+function _0x42ef() {
+  const _0x4f08c7 = [
+    "160Popwhv",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22pesanMasuk\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22nama\x22\x20style=\x22--color:\x20",
+    "6737465ggAELY",
+    "36ckXAPa",
+    "join",
+    "createElement",
+    "parentNode",
+    "split",
+    "addEventListener",
+    "namanya\x20gada",
+    "#17a2b8",
+    "d-none",
+    "setItem",
+    "classList",
+    "#dc3545",
+    "catch",
+    "959HvEAGw",
+    ";\x22>",
+    "querySelector",
+    "replace",
+    "add",
+    "</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20",
+    "&_=",
+    "toggle",
+    "POST",
+    "map",
+    "mau\x20ngirim\x20apa\x20coba",
+    "getElementById",
+    "</div>",
+    "length",
+    "<span\x20style=\x27text-decoration:\x20line-through;\x27>saya\x20mengganti\x20nama\x20secara\x20ilegal</span>",
+    "8117244vNYFLC",
+    "then",
+    "floor",
+    "#ffc107",
+    "11133947mzwCUM",
+    "button.active",
+    "innerHTML",
+    "3FBGqdj",
+    "https://docs.google.com/spreadsheets/d/",
+    "59344gSshIQ",
+    "\x22,\x22",
+    "1ZTCERAwio2qTbOKnyNpuZAYdcOZIuYpizSUDjswkJG0",
+    "div",
+    ".informasi",
+    "<span\x20style=\x27text-decoration:\x20line-through;\x27>users</span>",
+    "forms",
+    "nama\x20tidak\x20boleh\x20lebih\x20dari\x2025\x20kata.",
+    "submit",
+    "234198HCsCWz",
+    "value",
+    "pesan",
+    "getItem",
+    "Forum",
+    "text",
+    "1252MlmoAL",
+    "1920344HYxUBM",
+    "1993QkixBH",
+    "nama",
+    "preventDefault",
+    "<div\x20class=\x22areaPesanMasuk\x22>",
+    "textContent",
+    "slice",
+  ];
+  _0x42ef = function () {
+    return _0x4f08c7;
   };
-
-  if (window.innerWidth >= 576) {
-    kucing.addEventListener("mousedown", () => {
-      kucing.classList.remove("trans");
-      document.addEventListener("mousemove", move);
-    });
-
-    document.addEventListener("mouseup", () => {
-      kucing.style.bottom = "100%";
-      document.removeEventListener("mousemove", move);
-    });
-  }
-  if (window.innerWidth <= 576) {
-    let initialTouchY = 0;
-
-    kucing.addEventListener("touchstart", (event) => {
-      event.preventDefault(); // Mencegah scroll halaman saat menyentuh objek
-      kucing.classList.remove("bott");
-      const touch = event.touches[0];
-      initialTouchY = touch.clientY;
-    });
-
-    kucing.addEventListener("touchmove", (event) => {
-      event.preventDefault(); // Mencegah scroll halaman saat menggeser objek
-      const touch = event.touches[0];
-      const deltaY = touch.clientY - initialTouchY;
-      kucing.style.bottom = `calc(100% - ${deltaY}px)`;
-    });
-
-    kucing.addEventListener("touchend", () => {
-      // Tindakan setelah sentuhan selesai
-    });
-  }
-
-  document.body.appendChild(kucing);
-
-  setTimeout(() => {
-    kucing.classList.add("bott");
-    setTimeout(() => {
-      kucing.style.opacity = ".1";
-      setTimeout(() => {
-        kucing.parentNode.removeChild(kucing);
-      }, 3000);
-    }, 7000);
-  }, 1000);
+  return _0x42ef();
 }
-
-setInterval(createKucing, 20000);
+(function (_0x48718c, _0x55c156) {
+  const _0x3825d0 = _0x26b6,
+    _0x2e9337 = _0x48718c();
+  while (!![]) {
+    try {
+      const _0x1e8c4d =
+        (parseInt(_0x3825d0(0x6b)) / 0x1) * (-parseInt(_0x3825d0(0xa8)) / 0x2) +
+        (parseInt(_0x3825d0(0x97)) / 0x3) * (-parseInt(_0x3825d0(0xa9)) / 0x4) +
+        parseInt(_0x3825d0(0x73)) / 0x5 +
+        -parseInt(_0x3825d0(0x90)) / 0x6 +
+        (parseInt(_0x3825d0(0x81)) / 0x7) * (-parseInt(_0x3825d0(0x99)) / 0x8) +
+        (parseInt(_0x3825d0(0xa2)) / 0x9) * (parseInt(_0x3825d0(0x71)) / 0xa) +
+        (parseInt(_0x3825d0(0x94)) / 0xb) * (parseInt(_0x3825d0(0x74)) / 0xc);
+      if (_0x1e8c4d === _0x55c156) break;
+      else _0x2e9337["push"](_0x2e9337["shift"]());
+    } catch (_0x2f1a96) {
+      _0x2e9337["push"](_0x2e9337["shift"]());
+    }
+  }
+})(_0x42ef, 0xabc2c);
+const scriptURL =
+    "https://script.google.com/macros/s/AKfycbzZciEdywHsr62lneaD_35hh7doKpe0GrvJGjf_4ntU3jh_3Xx2KvePlZJ1CDhoKNWp/exec",
+  form = document[_0x201438(0x9f)][_0x201438(0xa6)],
+  btnActive = document[_0x201438(0x83)](_0x201438(0x95)),
+  btnDisable = document[_0x201438(0x83)]("button.disable"),
+  pesanForm = document[_0x201438(0x8c)](_0x201438(0xa4)),
+  namaForm = document[_0x201438(0x8c)](_0x201438(0x6c)),
+  namaLocal = localStorage["getItem"](_0x201438(0x6c));
+namaForm
+  ? (namaForm[_0x201438(0xa3)] = localStorage[_0x201438(0xa5)](_0x201438(0x6c)))
+  : (namaForm[_0x201438(0xa3)] = "");
+const informasi = document[_0x201438(0x83)](_0x201438(0x9d));
+function inf() {
+  const _0x120f00 = _0x201438;
+  informasi[_0x120f00(0x77)][_0x120f00(0x7e)]["remove"](_0x120f00(0x7c)),
+    setTimeout(() => {
+      const _0x33d3ec = _0x120f00;
+      informasi["parentNode"]["classList"][_0x33d3ec(0x85)]("d-none");
+    }, 0x1388);
+}
+function _0x26b6(_0xe7db7f, _0x503cb9) {
+  const _0x42ef79 = _0x42ef();
+  return (
+    (_0x26b6 = function (_0x26b636, _0x84826f) {
+      _0x26b636 = _0x26b636 - 0x6b;
+      let _0x5e8c57 = _0x42ef79[_0x26b636];
+      return _0x5e8c57;
+    }),
+    _0x26b6(_0xe7db7f, _0x503cb9)
+  );
+}
+form[_0x201438(0x79)](_0x201438(0xa1), (_0x6b8ddc) => {
+  const _0x540be4 = _0x201438;
+  _0x6b8ddc[_0x540be4(0x6d)]();
+  if (namaForm[_0x540be4(0xa3)] == "")
+    inf(), (informasi[_0x540be4(0x6f)] = _0x540be4(0x7a));
+  else {
+    if (namaForm[_0x540be4(0xa3)][_0x540be4(0x8e)] > 0x19)
+      inf(), (informasi[_0x540be4(0x6f)] = _0x540be4(0xa0));
+    else {
+      if (pesanForm[_0x540be4(0xa3)] == "")
+        inf(), (informasi[_0x540be4(0x6f)] = _0x540be4(0x8b));
+      else {
+        if (pesanForm[_0x540be4(0xa3)][_0x540be4(0x8e)] > 0x96)
+          inf(),
+            (informasi[_0x540be4(0x6f)] =
+              "Pesan\x20tidak\x20boleh\x20lebih\x20dari\x20150\x20kata.");
+        else {
+          function _0x404005() {
+            const _0x5dd997 = _0x540be4,
+              _0x46ee94 = [_0x5dd997(0x93), _0x5dd997(0x7f), _0x5dd997(0x7b)],
+              _0x20d03a = Math[_0x5dd997(0x92)](
+                Math["random"]() * _0x46ee94[_0x5dd997(0x8e)]
+              );
+            return _0x46ee94[_0x20d03a];
+          }
+          const _0x43bf25 = _0x404005(),
+            _0x2d6426 = document["getElementById"]("color");
+          _0x2d6426["value"] = _0x43bf25;
+          const _0x5c40c8 = pesanForm[_0x540be4(0xa3)],
+            _0x36fbad = _0x5c40c8[_0x540be4(0x84)](/[\n\r]/g, "\x20");
+          btnActive[_0x540be4(0x7e)][_0x540be4(0x88)](_0x540be4(0x7c)),
+            btnDisable[_0x540be4(0x7e)][_0x540be4(0x88)](_0x540be4(0x7c));
+          let _0x3f8c2 = namaForm[_0x540be4(0xa3)];
+          localStorage[_0x540be4(0x7d)](_0x540be4(0x6c), _0x3f8c2);
+          const _0x4af763 = new FormData(form);
+          _0x4af763["set"](_0x540be4(0xa4), _0x36fbad),
+            fetch(scriptURL, { method: _0x540be4(0x89), body: _0x4af763 })
+              [_0x540be4(0x91)]((_0x4ebf89) => {
+                const _0x48eea1 = _0x540be4;
+                btnActive[_0x48eea1(0x7e)][_0x48eea1(0x88)](_0x48eea1(0x7c)),
+                  btnDisable[_0x48eea1(0x7e)][_0x48eea1(0x88)](_0x48eea1(0x7c)),
+                  (pesanForm["value"] = "");
+              })
+              [_0x540be4(0x80)](
+                (_0x20e56e) => "ups\x20ada\x20yang\x20salah\x20🤔"
+              );
+        }
+      }
+    }
+  }
+});
+const spreadsheetId = _0x201438(0x9b);
+let url = _0x201438(0x98) + spreadsheetId + "/gviz/tq?tqx=out:csv";
+document[_0x201438(0x79)]("DOMContentLoaded", init);
+function init() {
+  fetchDataAndConvertToHTML(), setInterval(fetchDataAndConvertToHTML, 0x1388);
+}
+function fetchDataAndConvertToHTML() {
+  const _0x1e709f = _0x201438,
+    _0x20358e = new Date()["getTime"](),
+    _0x5570b1 = url + _0x1e709f(0x87) + _0x20358e;
+  fetch(_0x5570b1)
+    [_0x1e709f(0x91)]((_0x4e4541) => _0x4e4541[_0x1e709f(0xa7)]())
+    [_0x1e709f(0x91)]((_0x5ce4f1) => {
+      const _0x8da1eb = _0x1e709f,
+        _0xadcd1b = convertDataToHTML(_0x5ce4f1),
+        _0x2197d1 = document[_0x8da1eb(0x83)](".areaPesanMasuk");
+      _0x2197d1[_0x8da1eb(0x96)] = _0xadcd1b;
+    })
+    [_0x1e709f(0x80)]((_0x23bac3) => {
+      console["log"]("Terjadi\x20kesalahan:", _0x23bac3);
+    });
+}
+function convertDataToHTML(_0x1fe39d) {
+  const _0x1df9eb = _0x201438,
+    _0xb2ca02 = _0x1fe39d["split"]("\x0a")[_0x1df9eb(0x70)](0x1),
+    _0x1095b4 = _0xb2ca02[_0x1df9eb(0x8a)]((_0x3997b9) => {
+      const _0x423bb5 = _0x1df9eb,
+        _0x5232ec = _0x3997b9[_0x423bb5(0x78)](_0x423bb5(0x9a))[
+          _0x423bb5(0x8a)
+        ]((_0x415772) => _0x415772[_0x423bb5(0x84)](/"/g, "")),
+        [_0x308291, _0x54160f, _0x19200f, _0x47a75f] = _0x5232ec,
+        _0x246aed = document[_0x423bb5(0x76)](_0x423bb5(0x9c)),
+        _0x37a574 = document[_0x423bb5(0x76)](_0x423bb5(0x9c));
+      return (
+        _0x54160f == ""
+          ? ((_0x37a574[_0x423bb5(0x96)] = _0x423bb5(0x9e)),
+            (_0x246aed[_0x423bb5(0x96)] = _0x423bb5(0x8f)))
+          : ((_0x37a574[_0x423bb5(0x6f)] = _0x54160f),
+            (_0x246aed[_0x423bb5(0x6f)] = _0x19200f)),
+        _0x423bb5(0x72) +
+          _0x47a75f +
+          _0x423bb5(0x82) +
+          _0x37a574[_0x423bb5(0x96)] +
+          "</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22pesan\x22>" +
+          _0x246aed[_0x423bb5(0x96)] +
+          _0x423bb5(0x86)
+      );
+    })[_0x1df9eb(0x75)]("");
+  return _0x1df9eb(0x6e) + _0x1095b4 + _0x1df9eb(0x8d);
+}
